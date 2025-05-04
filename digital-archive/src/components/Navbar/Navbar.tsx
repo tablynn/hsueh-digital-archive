dad7f143b191d24e685be0cc6301d1c7ff771e4e
Shravya Sompalli <shravyasompalli@mac.mynetworksettings.com>
2025-05-04 18:22:07 -0400

    lowecase photos

--- digital-archive/src/components/Navbar/Navbar.tsx
+++ digital-archive/src/components/Navbar/Navbar.tsx
@@ -9,13 +9,13 @@ function Navbar() {
             <div className="navbar-nav">
                 <a className="nav-item nav-link active" href="/hsueh-digital-archive">Home</a>
                 <div className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" href="/Photographs">Photographs</a>
+                    <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" href="/photographs">Photographs</a>
                     <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a className="dropdown-item" href="/Photographs">All Photos</a>
+                        <a className="dropdown-item" href="/photographs">All Photos</a>
                         <div className="dropdown-divider"></div>
-                        <a className="dropdown-item" href="/Photographs#taiwan">Taiwan</a>
-                        <a className="dropdown-item" href="/Photographs#earlyUS">Early U.S.</a>
-                        <a className="dropdown-item" href="/Photographs#raisingKids">Raising a Family</a>
+                        <a className="dropdown-item" href="/photographs#taiwan">Taiwan</a>
+                        <a className="dropdown-item" href="/photographs#earlyUS">Early U.S.</a>
+                        <a className="dropdown-item" href="/photographs#raisingKids">Raising a Family</a>
                     </div>
                 </div>
                 <a className="nav-item nav-link" href="/themed-collections">Themed Collections</a>
